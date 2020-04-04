bf063dff1a67403ef329aee5d425cf54b4652188
Roger Esteve <Coconaut7@gmail.com>
2020-04-04 04:54:55 +0200

    Fixed issue #1

--- commands/cmd_skip.js
+++ commands/cmd_skip.js
@@ -4,7 +4,9 @@ module.exports = {
     short: "skip",
     exec: (msg, playData, args) => {
         if (!msg.member.voice.channel)
-        msg.channel.send("Pero que vas a skipejar si no estas a cap canal payaso.");
+            msg.channel.send("Pero que vas a skipejar si no estas a cap canal payaso.");
+        else if(!playData.playing)
+            msg.channel.send("Pero que vas a skipejar si no hi ha cap canço payaso.");
         else playData.connection.dispatcher.end();
         return playData;
     }
